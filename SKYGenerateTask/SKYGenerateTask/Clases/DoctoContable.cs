59f109cbcbd352c0cdcf5aad3fae7536de8b70bf
andres lopez <ralopezcastano51@gmail.com>
2020-07-09 16:07:27 -0500

    implementacion de serializador de json

---
+++ SKYGenerateTask/SKYGenerateTask/Clases/DoctoContable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKYGenerateTask.Clases
+{
+    class DoctoContable
+    {
+
+        public string F351_ID_AUXILIAR { get; set; }
+        public string F351_ID_TERCERO { get; set; }
+        public string F351_VALOR_DB { get; set; }
+        public string F351_VALOR_CR { get; set; }
+        
+    }
+}
--- SKYGenerateTask/SKYGenerateTask/Form1.cs
+++ SKYGenerateTask/SKYGenerateTask/Form1.cs
@@ -1,4 +1,6 @@
-﻿using SpreadsheetLight;
+﻿using Newtonsoft.Json;
+using SKYGenerateTask.Clases;
+using SpreadsheetLight;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -21,21 +23,34 @@ namespace SKYGenerateTask
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string filePath = @"C:\Users\Public\Documents\prueba2.xlsx";
+            string filePath = @"C:\Users\Public\Documents\prueba.xlsx";
              try
             {
-                SLDocument s1 = new SLDocument(filePath);
-
-           
-                int idRow = 1;
+                SLDocument s1 = new SLDocument(filePath);           
+                int idRow = 2;
+                List<DoctoContable> json = new List<DoctoContable>();
                 while (!string.IsNullOrEmpty(s1.GetCellValueAsString(idRow, 1)))
                 {
-                    var a = s1.GetCellValueAsString(idRow, 1);
-                    var b = s1.GetCellValueAsString(idRow, 2);
-                    var c = s1.GetCellValueAsString(idRow, 3);
+                    DoctoContable reg = new DoctoContable();
+
+                    reg.F351_ID_AUXILIAR = s1.GetCellValueAsString(idRow, 1);
+                    reg.F351_ID_TERCERO = s1.GetCellValueAsString(idRow, 2);
+
+                    var tipo = s1.GetCellValueAsString(idRow, 4);
+                    if (tipo == "db")                    
+                        reg.F351_VALOR_DB = s1.GetCellValueAsString(idRow, 3);
+                    else
+                        reg.F351_VALOR_CR = s1.GetCellValueAsString(idRow, 3);
+
+
+                    json.Add(reg);
                     idRow++;
                 }
-                    
+
+                string jsonDocto = JsonConvert.SerializeObject(json);
+                jsonDocto = @"[{""Conector"": ""Docto_Contable"", ""Movto_Contable"":" + jsonDocto+"}]";
+
+
             }
             catch (Exception ex)
             {
