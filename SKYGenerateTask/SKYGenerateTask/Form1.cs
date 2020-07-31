67e832a12d8c9776969e21f132d2194b381cafa6
andres lopez <ralopezcastano51@gmail.com>
2020-07-31 15:35:46 -0500

    ajustes en ruta de generacion y de destino - 20200731

--- SKYGenerateTask/SKYGenerateTask/Form1.cs
+++ SKYGenerateTask/SKYGenerateTask/Form1.cs
@@ -28,93 +28,100 @@ namespace SKYGenerateTask
             DataSet structure = new DataSet();
             PlaneBuilder planeBuild = new PlaneBuilder();
 
-            string filePath = @"C:\Users\Public\Documents\prueba.xlsx";//RUTA DONDE VA A LEER EXCEL
+            string filePath = @"C:\INTERFAZ TU COMPRA\Excel de cargue";//RUTA DONDE VA A LEER EXCEL
             try
             {
-                SLDocument s1 = new SLDocument(filePath);
-                int idRow = 2;
-                List<DoctoContable> jsonExcel = new List<DoctoContable>();
-
-                while (!string.IsNullOrEmpty(s1.GetCellValueAsString(idRow, 1)))//RECORRE EL EXCEL LEIDO
+                DirectoryInfo di = new DirectoryInfo(filePath);
+                foreach (var fi in di.GetFiles("*.xlsx"))
                 {
-                    DoctoContable reg = new DoctoContable();
+                    var file = filePath + @"\" + fi.Name;                                       
+                    SLDocument s1 = new SLDocument(file);
+                    int idRow = 2;
+                    List<DoctoContable> jsonExcel = new List<DoctoContable>();
 
-                    var cuenta = s1.GetCellValueAsString(idRow, 1);//VALIDA PARA QUE CUENTA SE VA A IR
-                    if (cuenta == "1")
+                    while (!string.IsNullOrEmpty(s1.GetCellValueAsString(idRow, 1)))//RECORRE EL EXCEL LEIDO
                     {
-                        reg.F351_ID_AUXILIAR = "11100503";
-                        reg.F351_ID_FE = "110201";
-                        reg.F351_ID_TERCERO = "";
-                        reg.F351_DOCTO_BANCO = "CG";
-                        reg.F351_NRO_DOCTO_BANCO = s1.GetCellValueAsString(idRow, 2);
+                        DoctoContable reg = new DoctoContable();
+
+                        var cuenta = s1.GetCellValueAsString(idRow, 1);//VALIDA PARA QUE CUENTA SE VA A IR
+                        if (cuenta == "11100503")
+                        {
+                            reg.F351_ID_AUXILIAR = "11100503";
+                            reg.F351_ID_FE = "110201";
+                            reg.F351_ID_TERCERO = "";
+                            reg.F351_DOCTO_BANCO = "CG";
+                            reg.F351_NRO_DOCTO_BANCO = s1.GetCellValueAsDateTime(idRow, 3).ToString("yyyyMMdd");
+                        }
+                        else if (cuenta == "53051502")
+                        {
+                            reg.F351_ID_AUXILIAR = "53051502";
+                            reg.F351_ID_FE = "";
+                            reg.F351_ID_TERCERO = "890300279";
+                            reg.F351_DOCTO_BANCO = "";
+                            reg.F351_NRO_DOCTO_BANCO = "0";
+                        }
+                        else
+                        {
+                            reg.F351_ID_AUXILIAR = "28050502";
+                            reg.F351_ID_FE = "";
+                            reg.F351_ID_TERCERO = "890300279";
+                            reg.F351_DOCTO_BANCO = "";
+                            reg.F351_NRO_DOCTO_BANCO = "0";
+                        }
+                        reg.F351_NOTAS = "SE RECLASIFICAN PAGOS TU COMPRA DEL DIA " + DateTime.Now.ToString("yyyyMMdd");
+
+                        var tipo = s1.GetCellValueAsString(idRow, 4);//VALIDA SI ES DEBITO O CREDITO
+                        if (tipo == "DB")
+                            reg.F351_VALOR_DB = s1.GetCellValueAsString(idRow, 2);
+                        else
+                            reg.F351_VALOR_CR = s1.GetCellValueAsString(idRow, 2);
+
+                        jsonExcel.Add(reg);
+                        idRow++;
                     }
-                    else if (cuenta == "2")
+                    string fecha = DateTime.Now.ToString("yyyyMMdd");
+                    fecha = @"""" + fecha + @"""";//COMILLA DOBLE EN FECHA
+                    string jsonDocto = JsonConvert.SerializeObject(jsonExcel);//CONVERSION OBJETO EN JSON
+                    jsonDocto = @"{ ""Conector"": ""Docto_Contable"",""F350_FECHA"":" + fecha //ADICION DE ENCABEZADO EN JSON DE DOCTO CONTABLE
+                              + @",""F350_NOTAS"": ""SE RECLASIFICAN PAGOS TU COMPRA DEL DIA " + DateTime.Now.ToString("yyyyMMdd")
+                              + @" "",""Movto_Contable"":" + jsonDocto + "}";
+
+                    structure = ejecutar.GetStruct();//CONSULTA LA ESTRUCTURA
+
+                    StringBuilder plane = new StringBuilder();
+                    int consectLine = 1;
+                    JObject jsonValue = JObject.Parse(jsonDocto);
+                    List<JObject> value = new List<JObject>();
+                    value.Add(jsonValue);
+
+                    if (value != null)//ARMA EL PLANO CON BASE EN EL JSON Y LA ESTRUCTURA
                     {
-                        reg.F351_ID_AUXILIAR = "53051502";
-                        reg.F351_ID_FE = "";
-                        reg.F351_ID_TERCERO = "890300279";
-                        reg.F351_DOCTO_BANCO = "";
-                        reg.F351_NRO_DOCTO_BANCO = "0";
+                        plane.Append(planeBuild.BuildInitial(structure, value[0]));//construye linea inicial
+
+                        for (int j = 0; j < value.Count; j++)//recorre la lista de registros a enviar
+                        {
+
+                            string ConectorType = (string)value[j]["Conector"];//extrae el nombre del conector
+                            JObject json = value[j];//extrae json del conector a enviar
+
+                            plane.Append(planeBuild.BuildMasters(structure, json, ref consectLine));//construye encabezados o maestros
+                            string Pano = plane.ToString();
+                            plane.Append(planeBuild.BuildDetails(structure, json, ref consectLine));//construye movimientos
+
+
+                        }
+
+                        plane.Append(planeBuild.BuildFinal(structure, value[0], ref consectLine));//construye linea final
+
+
+                        string Plano = plane.ToString();
+                        var SavePlane = new StreamWriter($@"C:\Txt\Unoee\{fi.Name}.txt");
+                        SavePlane.WriteLine(Plano);
+                        SavePlane.Close();
+
+                        fi.MoveTo($@"C:\INTERFAZ TU COMPRA\Excel de cargue\procesados\{fi.Name}");
+                        
                     }
-                    else
-                    {
-                        reg.F351_ID_AUXILIAR = "28050502";
-                        reg.F351_ID_FE = "";
-                        reg.F351_ID_TERCERO = "890300279";
-                        reg.F351_DOCTO_BANCO = "";
-                        reg.F351_NRO_DOCTO_BANCO = "0";
-                    }
-                    reg.F351_NOTAS = "SE RECLASIFICAN PAGOS TU COMPRA DEL DIA " + DateTime.Now.ToString("yyyyMMdd");             
-                    
-                    var tipo = s1.GetCellValueAsString(idRow, 4);//VALIDA SI ES DEBITO O CREDITO
-                    if (tipo == "db")
-                        reg.F351_VALOR_DB = s1.GetCellValueAsString(idRow, 3);
-                    else
-                        reg.F351_VALOR_CR = s1.GetCellValueAsString(idRow, 3);
-
-                    jsonExcel.Add(reg);
-                    idRow++;
-                }
-                string fecha = DateTime.Now.ToString("yyyyMMdd");
-                fecha = @"""" + fecha + @"""";//COMILLA DOBLE EN FECHA
-                string jsonDocto = JsonConvert.SerializeObject(jsonExcel);//CONVERSION OBJETO EN JSON
-                jsonDocto = @"{ ""Conector"": ""Docto_Contable"",""F350_FECHA"":" + fecha //ADICION DE ENCABEZADO EN JSON DE DOCTO CONTABLE
-                          + @",""F350_NOTAS"": ""SE RECLASIFICAN PAGOS TU COMPRA DEL DIA "+ DateTime.Now.ToString("yyyyMMdd")
-                          + @" "",""Movto_Contable"":" + jsonDocto + "}";
-
-                structure = ejecutar.GetStruct();//CONSULTA LA ESTRUCTURA
-
-                StringBuilder plane = new StringBuilder();
-                int consectLine = 1;
-                JObject jsonValue = JObject.Parse(jsonDocto);
-                List<JObject> value = new List<JObject>();
-                value.Add(jsonValue);
-
-                if (value != null)//ARMA EL PLANO CON BASE EN EL JSON Y LA ESTRUCTURA
-                {
-                    plane.Append(planeBuild.BuildInitial(structure, value[0]));//construye linea inicial
-
-                    for (int j = 0; j < value.Count; j++)//recorre la lista de registros a enviar
-                    {
-
-                        string ConectorType = (string)value[j]["Conector"];//extrae el nombre del conector
-                        JObject json = value[j];//extrae json del conector a enviar
-
-                        plane.Append(planeBuild.BuildMasters(structure, json, ref consectLine));//construye encabezados o maestros
-                        string Pano = plane.ToString();
-                        plane.Append(planeBuild.BuildDetails(structure, json, ref consectLine));//construye movimientos
-
-
-                    }
-
-                    plane.Append(planeBuild.BuildFinal(structure, value[0], ref consectLine));//construye linea final
-
-
-                    string Plano = plane.ToString();
-                    var SavePlane = new StreamWriter($@"C:\Users\Public\Documents\DoctoContable{DateTime.Now.ToString("ddMMyyyy")}.txt");
-                    SavePlane.WriteLine(Plano);
-                    SavePlane.Close();
-
                 }
 
 
